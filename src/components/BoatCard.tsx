5bca9cc7aa9c4a5bc419b948f1eda653babc5281
Leonardo Dalcin <leon.dalcin@gmail.com>
2020-07-12 15:03:42 -0300

    feat: adds colors

--- src/components/BoatCard.tsx
+++ src/components/BoatCard.tsx
@@ -8,11 +8,15 @@ export default function BoatCard() {
     const boat = data?.getBoats[0]
     return (
       <div className="BoatCard-grid">
-        <img src={boat.imageUrl!} alt="Boat"></img>
+        <div style={{backgroundImage: `url(${boat.imageUrl})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        height:"100%"}}></div>
         <div className={'Info-grid'}>
           {/*left side*/}
           <div>
-            <div>{boat.type} / {boat.length} / {boat.name} / {boat.year}</div>
+            <div>{boat.name} / {boat.year}</div>
             <div>{boat.reviews!.score} / Read Reviews</div>
             <div>Skipper: {boat.skipper}</div>
             <div>Cabins: {boat.cabins}</div>
